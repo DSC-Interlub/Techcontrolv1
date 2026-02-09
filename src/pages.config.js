05e82ee9a42f776f53207b639d019a709ad1f6c3
Kauan Kubia <kkubia797@gmail.com>
2026-02-09 19:02:13 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -66,7 +66,6 @@ import Usuarios from './pages/Usuarios';
 import acompanharChamado from './pages/acompanhar-chamado';
 import chamadoPublico from './pages/chamado-publico';
 import reservaPublica from './pages/reserva-publica';
-import avaliacoesEquipamentos from './pages/Avaliacoes_Equipamentos';
 import __Layout from './Layout.jsx';
 
 
@@ -90,7 +89,6 @@ export const PAGES = {
     "acompanhar-chamado": acompanharChamado,
     "chamado-publico": chamadoPublico,
     "reserva-publica": reservaPublica,
-    "Avaliacoes_Equipamentos": avaliacoesEquipamentos,
 }
 
 export const pagesConfig = {
